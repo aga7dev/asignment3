28a8eb121fe938f380c9f39b4fc60f56df782f37
Agadil <agadil.altaev@list.ru>
2026-01-24 15:27:24 +0500

    added database connection

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/car_rental_db";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "2026";
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
